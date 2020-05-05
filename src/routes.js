1ff15941e9e7c844e7e07f4240e7018c2d796259
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-05-04 22:47:37 -0300

    finish css header, body and footer to create product.

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,6 @@ routes.get('/', (req,res) => {
 })
 
 routes.get('/products/create', ProductController.create)
-routes.post('/products', ProductController.post)
-
 
 routes.get('/ads/create', (req,res) => {
     return res.redirect("/products/create")
